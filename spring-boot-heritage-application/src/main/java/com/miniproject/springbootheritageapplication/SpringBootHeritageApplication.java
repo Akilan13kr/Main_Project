75f507c84f51119c5b3ac5ac0e6203b01076bad2
KRAkilan <131351948+Akilan13kr@users.noreply.github.com>
2024-03-15 20:21:50 +0530

    Add files via upload
    1st time upload in the git

---
+++ spring-boot-heritage-application/src/main/java/com/miniproject/springbootheritageapplication/SpringBootHeritageApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.springbootheritageapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHeritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHeritageApplication.class, args);
+	}
+
+}
---
+++ spring-boot-heritage-application/src/test/java/com/miniproject/springbootheritageapplication/SpringBootHeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.springbootheritageapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHeritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
